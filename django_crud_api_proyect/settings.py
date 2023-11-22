8c064a1119b8481f1317a5fd960dbebf64dfd43f
wGuerrero333 <wguerreroc1969@gmail.com>
2023-11-22 13:43:19 -0500

    Añadido cosrs todo ok listo para agregar FRONTED

--- django_crud_api_proyect/settings.py
+++ django_crud_api_proyect/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'coreapi',
     'apps'
 ]
@@ -45,7 +46,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    
+     "corsheaders.middleware.CorsMiddleware",
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -129,3 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
+# autorizacion de poder conectar dos puertos backend y fronted
+CORS_ALLOWED_ORIGINS = []
